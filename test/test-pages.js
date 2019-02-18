5da2e322324aab343df3261b7258a09df29ba54c
rupesh kumar <rupesh.kumar@adglobal360.com>
2019-02-18 18:48:33 +0530

    add testing with mocha

--- test/test-pages.js
+++ test/test-pages.js
@@ -3,14 +3,15 @@ var request = require('request');
 
 it('Import CSV', function(done) {
     request('http://localhost:3000/api/v1/import' , function(error, response, body) {
-        expect(body).to.equal('{"success":true,"result":"result"}');
+    	console.log(body);
+        expect(response.statusCode).to.equal(200);
         done();
     });
 });
 
 it('Assign requset', function(done) {
     request('http://localhost:3000/api/v1/assignButler' , function(error, response, body) {
-        expect(body).to.equal('{"success":true,"result":{"butlers":[{"requests":["abc","ghi"]},{"requests":["abc","zzz"]},{"requests":["ghi","def"]},{"requests":["ghi","zzz"]},{"requests":["def","zzz"]}],"spreadClientIds":[1,2]}}');
+        expect(response.statusCode).to.equal(200);
         done();
     });
 });
